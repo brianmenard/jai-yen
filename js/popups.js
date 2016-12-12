19bc2ad4ca292ad78dea570893723e70977a1261
Brian Menard <hello@webdevbrian.com>
2016-12-12 20:02:01 +0000

    added popups for testimonials

--- js/popups.js
+++ js/popups.js
@@ -19,5 +19,35 @@
 	        pagecontainer: '.container',
 	        transition: 'all 0.3s'
 	    });
+	    
+	    $('#test1').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#test2').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#test3').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#test4').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#test5').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#test6').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
 	
 	});
