558c3588ecdda9b2d68fb0b22df11f0ff91320c7
Brian Menard <hello@webdevbrian.com>
2016-12-12 19:30:16 +0000

    moved popup styling and scripts to repective separate files rather than in-line.  updated form styling

---
+++ js/popups.js
@@ -0,0 +1,23 @@
+	$(document).ready(function () {
+	
+	    $('#bill').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#jim').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#chris').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	    
+	    $('#janet').popup({
+	        pagecontainer: '.container',
+	        transition: 'all 0.3s'
+	    });
+	
+	});
